6f281fa3771d1043f9e1a1d5e35843afc391baf9
Phillip Bailey <ztron500@gmail.com>
2024-11-15 20:35:05 -0500

    update

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import HeroHome from "./components/HeroHome";
+import Navbar from "./components/Navbar";
+
+
+export default function Home() {
+  return (
+    <>
+     <Navbar/> 
+     <HeroHome/>     
+    </>
+  );
+}
